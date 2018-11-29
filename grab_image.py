07d1a4c4379dd9796545db350f520fb994f59b65
Jesper Stockenstrand <jesper.stockenstrand@dirac.com>
2018-11-29 15:03:58 +0100

    Fix

--- grab_image.py
+++ grab_image.py
@@ -8,10 +8,12 @@ def grab_image():
     io_stream = io.BytesIO()
     camera = PiCamera()
     camera.resolution = (3280,2464)
+    print("#Turn on LED")
     camera.start_preview()
     sleep(2)
     camera.capture(io_stream, 'jpeg')
     camera.stop_preview()
+    print("#Turn off LED")
     io_stream.seek(0)
     return io_stream
     
--- start.py
+++ start.py
@@ -1,9 +1,14 @@
 import io
 import img_proc
 import grab_image
-
+import solver as sv
 
 
 cube_side = grab_image.grab_image()
 cube_string = img_proc.list_colors(cube_side)
+#Turn cube and loop
+
+
+#a = sv.solve(cube_string, 20, 2)
 print(cube_string)
+#print(a)
